4cc016630cdbb5abf372c48417ef5a3803d6bddc
Matthew King <mattking73@gmail.com>
2024-12-09 06:53:17 -1000

    Day 2 part 2

--- day2/index.ts
+++ day2/index.ts
@@ -1,28 +1,37 @@
 import { run } from '../advent.ts';
 
-function isSafe(line: string) {
-    const nums = line.split(' ').map((s) => Number(s));
+function isSafeStill(nums: number[], curr: number) {
+    const isTrendingTheSame =
+        curr + 2 >= nums.length || nums[curr] > nums[curr + 1] === nums[curr + 1] > nums[curr + 2];
+    const diff = Math.abs(nums[curr] - nums[curr + 1]);
 
-    for (let i = 1; i < nums.length; i++) {
-        const last = nums[i - 1];
-        const curr = nums[i];
-        const diff = Math.abs(last - curr);
-        if (diff < 1 || diff > 3) {
-            return false;
-        }
+    return isTrendingTheSame && diff >= 1 && diff <= 3;
+}
 
-        if (i === 1) {
-            continue;
-        }
-
-        const lastIncreasing = last > nums[i - 2];
-        const currentIncreasing = curr > last;
-        if (lastIncreasing !== currentIncreasing) {
+function numsAreSafe(nums: number[]) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (!isSafeStill(nums, i)) {
             return false;
         }
     }
 
     return true;
+}
+
+function isSafe(line: string, { dampen } = { dampen: false }) {
+    const nums = line.split(' ').map((s) => Number(s));
+
+    const result = numsAreSafe(nums);
+    if (dampen && !result) {
+        for (let i = 0; i < nums.length; i++) {
+            const numsWithoutCurrentIndex = nums.toSpliced(i, 1);
+
+            if (numsAreSafe(numsWithoutCurrentIndex)) {
+                return true;
+            }
+        }
+    }
+    return result;
 }
 
 // 341
@@ -33,4 +42,14 @@ Safe if:
 */
 run(2, ({ lines }) => {
     return lines.filter(Boolean).filter((line) => isSafe(line)).length;
+});
+
+// 404
+/*
+Now, the same rules apply as before, except if removing a single 
+level from an unsafe report would make it safe, the report instead 
+counts as safe.
+*/
+run(4, ({ lines }) => {
+    return lines.filter(Boolean).filter((line) => isSafe(line, { dampen: true })).length;
 });
