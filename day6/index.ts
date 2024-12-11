71382d22c5a75a7e77441307644f8248cc1a8dd2
Matthew King <mattking73@gmail.com>
2024-12-11 07:33:18 -1000

    Day 6 part 2

--- day6/index.ts
+++ day6/index.ts
@@ -96,3 +96,126 @@ run(41, ({ lines }) => {
 
     return total;
 });
+
+/*
+    In the above example, there are only 6 different positions where a new 
+    obstruction would cause the guard to get stuck in a loop. 
+    The diagrams of these six situations use O to mark the new obstruction, 
+    | to show a position where the guard moves up/down, 
+    - to show a position where the guard moves left/right, and 
+    + to show a position where the guard moves both up/down and left/right.
+
+    It doesn't really matter what you choose to use as an obstacle so long as you 
+    and The Historians can put it into position without the guard noticing. 
+    The important thing is having enough options that you can find one that minimizes 
+    time paradoxes, and in this example, there are 6 different positions you could choose.
+*/
+// 1309
+run(6, ({ lines }) => {
+    // Can't be at `startPosition`
+
+    /*
+        Naive solution:
+
+        for each row/col:
+            start up/down/left/right
+            see if it creates loop (hits row/col again)
+    */
+
+    let total = 0;
+    let turns = [];
+    const [grid, startPosition] = gridify(lines);
+    const [startRow, startCol, dir] = startPosition;
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[row].length; col++) {
+            const phreshGrid = JSON.parse(JSON.stringify(grid));
+            phreshGrid[row][col] = '#';
+            const turn = isLoop(phreshGrid, startRow, startCol, dir);
+            if (turn) {
+                turns.push(turn);
+                total += 1;
+            }
+        }
+    }
+    return total;
+});
+
+function isLoop(grid: string[][], row: number, col: number, dir: string) {
+    let isLoop = false;
+    const turns: string[] = [];
+
+    function turn() {
+        grid[row][col] = '+';
+
+        const turn = `${row}-${col}`;
+        const existingTurnIndex = turns.indexOf(turn);
+        const all = turns.slice(existingTurnIndex);
+        if (all.length > 4) {
+            return all;
+        }
+        turns.push(turn);
+    }
+
+    while (row > -1 && row < grid.length && col > -1 && col < grid[0].length) {
+        if (dir === 'up') {
+            if (row === 0) {
+                break;
+            }
+            if (grid[row - 1][col] === '#') {
+                dir = 'right';
+                const loop = turn();
+                if (loop) {
+                    return loop;
+                }
+            } else {
+                row--;
+                grid[row][col] = '|';
+            }
+        } else if (dir === 'right') {
+            if (col === grid[0].length - 1) {
+                break;
+            }
+
+            if (grid[row][col + 1] === '#') {
+                dir = 'down';
+                const loop = turn();
+                if (loop) {
+                    return loop;
+                }
+            } else {
+                col++;
+                grid[row][col] = '-';
+            }
+        } else if (dir === 'down') {
+            if (row === grid.length - 1) {
+                break;
+            }
+            if (grid[row + 1][col] === '#') {
+                dir = 'left';
+                const loop = turn();
+                if (loop) {
+                    return loop;
+                }
+            } else {
+                row++;
+                grid[row][col] = '|';
+            }
+        } else if (dir === 'left') {
+            if (col === 0) {
+                break;
+            }
+            if (grid[row][col - 1] === '#') {
+                dir = 'up';
+                const loop = turn();
+                if (loop) {
+                    return loop;
+                }
+            } else {
+                col--;
+                grid[row][col] = '-';
+            }
+        }
+    }
+
+    return isLoop;
+}
